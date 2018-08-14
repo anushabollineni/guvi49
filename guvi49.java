579db4321d9eb10bf651280dc156b1b50ddeea60
anushabollineni <41730220+anushabollineni@users.noreply.github.com>
2018-08-14 11:02:17 +0530

    Create guvi49.java

---
+++ guvi49.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int num1 = 5, num2 = 5, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   
+	}
+}
